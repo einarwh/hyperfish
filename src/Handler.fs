73e3a794287d5ef638b3607f1c9df88ee81ebd7d
Einar W. Høst <einarwh@gmail.com>
2021-05-11 10:33:05 +0200

    Transforms.

--- src/Handler.fs
+++ src/Handler.fs
@@ -11,36 +11,98 @@ open Styling
 open Fishier
 open Letters
 open Figures
+open Stack
 
-let choosePicture (name : string) : Picture = 
-    let shapes = 
+exception TypeException of string
+
+let tryLookupPicture (name : string) : Picture option = 
+    let maybeShapes = 
         match name with 
-        | "/fish" -> fishShapes 
-        | "/george" -> georgeShapes
-        | "/f-letter" -> fLetter
-        | "/h-letter" -> hLetter
-        | "/e-letter" -> eLetter
-        | "/n-letter" -> nLetter
-        | "/d-letter" -> dLetter
-        | "/r-letter" -> rLetter
-        | "/s-letter" -> sLetter
-        | "/o-letter" -> oLetter
-        | _ -> fishShapes
-    createLensPicture shapes
+        | "blank" -> Some []
+        | "fish" -> Some fishShapes 
+        | "george" -> Some georgeShapes
+        | "f-letter" -> Some fLetter
+        | "h-letter" -> Some hLetter
+        | "e-letter" -> Some eLetter
+        | "n-letter" -> Some nLetter
+        | "d-letter" -> Some dLetter
+        | "r-letter" -> Some rLetter
+        | "s-letter" -> Some sLetter
+        | "o-letter" -> Some oLetter
+        | _ -> None
+    maybeShapes |> Option.map (fun shapes -> createLensPicture shapes) 
 
-let render (s : string) : HttpHandler = 
-    //(bounds, background, shapes) 
+let tryParsePictureValue (s : string) : StackValue option =
+    s |> tryLookupPicture |> Option.map PictureValue 
+
+let tryParseNumber (str : string) : int option =
+    match System.Int32.TryParse str with
+    | (true, n) -> Some n
+    | _ -> None
+
+let tryParseNumberValue (s : string) : StackValue option = 
+    s |> tryParseNumber |> Option.map NumberValue
+
+let transformCode (transform : Picture -> Picture) : Stack -> Stack = 
+    fun stack ->    
+        match stack with 
+        | [] -> raise (StackUnderflowException)
+        | PictureValue p :: restStack -> PictureValue (transform p) :: restStack
+        | _ -> raise (TypeException "Expected a picture on the stack")
+
+let tryLookupFunction (name : string) : Function option =
+    match name with 
+    | "turn" -> 
+        Some { Name = "turn"; Code = transformCode turn } 
+    | "flip" -> 
+        Some { Name = "flip"; Code = transformCode flip } 
+    | "toss" -> 
+        Some { Name = "toss"; Code = transformCode toss } 
+    | _ -> None
+
+let tryParseFunctionValue (s : string) : StackValue option =
+    s |> tryLookupFunction |> Option.map FunctionValue
+
+let parseStackValue (s : string) : StackValue =
+    let parseResult = 
+        tryParseNumberValue s
+          |> Option.orElse (tryParsePictureValue s)
+          |> Option.orElse (tryParseFunctionValue s)
+    match parseResult with 
+    | Some v -> v 
+    | None -> failwith <| sprintf "Invalid value %s" s  
+
+let rec tryFindFirstPicture (stack : Stack) : Picture option = 
+    match stack with 
+    | [] -> None 
+    | PictureValue p :: t -> Some p 
+    | _ :: t -> tryFindFirstPicture t
+
+let fooStuff values = 
+    let stk = runProgram [] values
+    let maybePicture = tryFindFirstPicture stk 
     let bounds = (600, 600)
     let background = Grey
-    let picture = choosePicture s
-    let box = { a = { x = 100.; y = 100. }
-                b = { x = 400.; y = 0. }
-                c = { x = 0.; y = 400. } }
-    let lens = (box, Blackish)
-    let shapes = picture lens
-    let v = view (bounds, background, shapes)
-    let result = renderHtmlDocument v
-    htmlString result
+    match maybePicture with 
+    | Some picture -> 
+        let box = { a = { x = 100.; y = 100. }
+                    b = { x = 400.; y = 0. }
+                    c = { x = 0.; y = 400. } }
+        let lens = (box, Blackish)
+        let shapes = picture lens
+        let v = view (bounds, background, shapes)
+        let result = renderHtmlDocument v
+        htmlString result
+    | None -> htmlString "Nothing here."
+
+
+let render (s : string) : HttpHandler = 
+    let strs = s.Split("/") |> List.ofArray |> List.tail
+    try 
+        let values = strs |> List.map parseStackValue
+        fooStuff values
+    with 
+    | _ -> htmlString "bad stuff"
 
 let escherHandler (matches : string seq) : HttpHandler =
     fun (next : HttpFunc) (ctx : HttpContext) ->
--- src/Stack.fs
+++ src/Stack.fs
@@ -1,36 +1,46 @@
 module Stack 
 
-  exception StackUnderflowException
+open Shade
 
-  type Stack = StackValue list
-  and StackValue = 
-  | PictureValue of Picture 
-  | FunctionValue of Function 
-  | NumberValue of int 
-  and Picture = Picture 
-  and Function = {
-    Name : string 
-    Code : Stack -> Stack
-  }
+exception StackUnderflowException
 
-  let create() : Stack = []
+type Stack = StackValue list
+and StackValue = 
+| PictureValue of Picture 
+| FunctionValue of Function 
+| NumberValue of int 
+and Function = {
+  Name : string 
+  Code : Stack -> Stack
+}
 
-  let pop stack : Stack = 
-    match stack with
-    | [] -> raise StackUnderflowException 
-    | _ :: t -> t
+let create() : Stack = []
 
-  let evaluate (stack : Stack) (fn : Function) : Stack = 
-    match fn with 
-    | { Name = _
-        Code = code } ->
-      code stack
+let top stack : StackValue = 
+  match stack with
+  | [] -> raise StackUnderflowException 
+  | h :: t -> h
 
-  let push (stack : Stack) (v : StackValue) : Stack = 
-    match v with 
-    | NumberValue _ -> v :: stack 
-    | PictureValue _ -> v :: stack 
-    | FunctionValue f -> evaluate stack f
+let pop stack : Stack = 
+  match stack with
+  | [] -> raise StackUnderflowException 
+  | _ :: t -> t
 
+let evaluate (stack : Stack) (fn : Function) : Stack = 
+  match fn with 
+  | { Name = _
+      Code = code } ->
+    code stack
 
+let push (stack : Stack) (v : StackValue) : Stack = 
+  printfn "pushing value %A" v
+  match v with 
+  | NumberValue _ -> v :: stack 
+  | PictureValue _ -> v :: stack 
+  | FunctionValue f -> evaluate stack f
 
+let rec runProgram (stack : Stack) (values : StackValue list) : Stack = 
+  match values with 
+  | [] -> stack
+  | v :: vs -> 
+    runProgram (push stack v) vs
